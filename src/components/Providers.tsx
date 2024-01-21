33b4e403bc1427b4d224f5e3d7a26837d7453c5e
aniket-tatte <aniket.tatte15@gmail.com>
2024-01-22 00:30:54 +0530

    absolute url fix for vercel deployment

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,24 +1,28 @@
-'use client'
+"use client";
 
-import { PropsWithChildren, useState } from "react"
+import { trpc } from "@/app/_trpc/client";
+import { absoluteUrl } from "@/lib/utils";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "@/app/_trpc/client"
-import { httpBatchLink } from "@trpc/client"
+import { httpBatchLink } from "@trpc/client";
+import { PropsWithChildren, useState } from "react";
 
-const Providers = ({children}: PropsWithChildren) => {
-    const [queryClient] = useState( () => new QueryClient())
-    const [trpcClient] = useState(() =>
-      trpc.createClient({
-        links: [httpBatchLink({
-            url: 'http://localhost:3000/api/trpc'
-        })],
-      })
-    );
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: absoluteUrl("/api/trpc"),
+        }),
+      ],
+    })
+  );
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
   );
-}
+};
 
-export default Providers
+export default Providers;
