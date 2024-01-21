f8fbbf4abd1bb1a011339505f2ff976032d41c1c
aniket-tatte <aniket.tatte15@gmail.com>
2024-01-21 12:49:08 +0530

    feature: Render and Delete Files

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,8 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { publicProcedure, privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import {z} from "zod";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -26,7 +27,33 @@ export const appRouter = router({
     }
 
     return {success: true};
-  })
+  }),
+  getUserFiles: privateProcedure.query(async ({ctx})=>{
+    const {user, userId} = ctx;
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    })
+  }),
+  deleteFile: privateProcedure.input(
+    z.object({id: z.string()})
+   ).mutation(async  ({ctx, input})=>{
+    const {userId} = ctx;
+    const file = await db.file.findFirst({
+      where: {
+        id: input.id,
+        userId: userId
+      }
+    })
+    if (!file) throw new TRPCError({code: 'NOT_FOUND'})
+    await db.file.delete({
+      where: {
+        id: input.id
+      }
+    })
+    return file;
+   }),
 });
 
 export type AppRouter = typeof appRouter;
